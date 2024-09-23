7b17c2a7bc34e559100396139727e4bb3260af32
Rudolf31 <rudolfshevchenko@gmail.com>
2024-09-23 23:03:20 +0300

    were added new files of controllers

--- services/database/init_db.py
+++ services/database/init_db.py
@@ -1,41 +1,8 @@
-import aiosqlite
+from peewee import SqliteDatabase
 from config import DATABASE
+from services.database.models import User, Employee, Reminder, Subscriber
+
+db = SqliteDatabase(DATABASE)
 
 async def init_db():
-
-    global DATABASE
-
-    async with aiosqlite.connect(DATABASE) as db:
-        await db.execute('''
-            CREATE TABLE IF NOT EXISTS Users (
-                id INTEGER PRIMARY KEY AUTOINCREMENT,
-                tg_id BIGINT NOT NULL,
-                admin BOOLEAN
-            )
-        ''')
-        await db.execute('''
-            CREATE TABLE IF NOT EXISTS Employees (
-                id INTEGER PRIMARY KEY AUTOINCREMENT,
-                full_name TEXT NOT NULL,
-                birthday TIMESTAMP NOT NULL,
-                tg_id BIGINT
-            )
-        ''')
-        await db.execute('''
-            CREATE TABLE IF NOT EXISTS Reminders (
-                id INTEGER PRIMARY KEY AUTOINCREMENT,
-                user_id INTEGER NOT NULL,
-                employee_id INTEGER NOT NULL,
-                
-                FOREIGN KEY (user_id) REFERENCES Users(id),
-                FOREIGN KEY (employee_id) REFERENCES Employees(id)
-            )
-        ''')
-        await db.execute('''
-            CREATE TABLE IF NOT EXISTS Subscribers (
-                id INTEGER PRIMARY KEY AUTOINCREMENT,
-                user_id INTEGER NOT NULL UNIQUE,
-                FOREIGN KEY (user_id) REFERENCES Users(id)
-            )
-        ''')
-        await db.commit()
+    await db.create_tables([User, Employee, Reminder, Subscriber])
