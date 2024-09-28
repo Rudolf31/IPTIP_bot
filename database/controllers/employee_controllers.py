56a0eb8e5b9da066e04279fe14a147c634ef92ac
Rudolf31 <rudolfshevchenko@gmail.com>
2024-09-28 20:40:43 +0300

    finished employees_controllers

--- database/controllers/employee_controllers.py
+++ database/controllers/employee_controllers.py
@@ -5,4 +5,43 @@ from database.application_context import Employee
 class EmployeeController():
     async def addEmployee(employee) -> Employee:
 
-        raise NotImplementedError
+        async with AppContext() as database:
+            with database.atomic():
+                
+                new_employee = Employee.create(full_name=employee.full_name, birthday=employee.birthday, tg_id=employee.tg_id)
+                new_employee.save()
+                return new_employee
+
+
+    async def getEmployeeById(id) -> Employee:
+
+        async with AppContext() as database:
+            with database.atomic():
+
+                return Employee.get(Employee.id == id)
+
+    async def getEmployeeByTgId(tg_id) -> Employee:
+
+        async with AppContext() as database:
+            with database.atomic():
+
+                return Employee.get(Employee.tg_id == tg_id)
+
+
+    async def deleteEmployee(id) -> bool:
+
+        async with AppContext() as database:
+            with database.atomic():
+
+                employee = Employee.get(Employee.id == id)
+                employee.delete_instance()
+                return True
+
+
+    async def updateEmployee(employee) -> bool:
+
+        async with AppContext() as database:
+            with database.atomic():
+
+                employee.save()
+                return True
