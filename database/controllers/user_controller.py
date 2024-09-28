2955ac43db3d9da64d66bb90f20a947030c90d3e
Rudolf31 <rudolfshevchenko@gmail.com>
2024-09-28 19:23:07 +0300

    finished user controller

--- database/controllers/user_controller.py
+++ database/controllers/user_controller.py
@@ -9,3 +9,30 @@ class UserController():
             new_user = User.create(tg_id=user.tg_id, admin=user.admin)
             new_user.save()
             return new_user
+
+
+    async def getUserById(id) -> User:
+
+        async with AppContext() as database:
+            return User.get(User.id == id)
+
+
+    async def getUserByTgId(tg_id) -> User:
+
+        async with AppContext() as database:
+            return User.get(User.tg_id == tg_id)
+
+    
+    async def delUser(id) -> bool:
+
+        async with AppContext() as database:
+            user = User.get(User.id == id)
+            user.delete_instance()
+            return True
+
+    
+    async def updateUser(user) -> bool:
+
+        async with AppContext() as database:
+            user.save()
+            return True
