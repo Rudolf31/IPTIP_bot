dd58d49d4618540b6b39490493ecd5049e5e75ff
Rudolf31 <55095427+Rudolf31@users.noreply.github.com>
2024-09-29 21:14:35 +0300

    Revert "Controller logic"

--- __main__.py
+++ __main__.py
@@ -1,12 +1,19 @@
+from services.bot_service import run_bot
 import logging
 import sys
 import asyncio
+from database.application_context import AppContext, User
+from database.controllers.user_controller import UserController
 
-from services.bot_service import run_bot
+
+# TODO: remove when done with tinkering
+async def test():
+    pass
 
 
 def main() -> None:
     logging.basicConfig(level=logging.INFO, stream=sys.stdout)
+    asyncio.run(test())
     asyncio.run(run_bot())
 
 
