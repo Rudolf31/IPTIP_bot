9ce746c0740488ce3c6649acfef60dcfafe2f842
Nikolai Papin <nikolai@weirdcat.ru>
2024-09-29 21:00:43 +0300

    Reminder controller prettified

--- database/controllers/reminder_controller.py
+++ database/controllers/reminder_controller.py
@@ -1,9 +1,19 @@
 from database.application_context import AppContext
-from database.application_context import Reminder, User, Employee
+from database.application_context import Reminder
+
 
 class ReminderController():
+    """
+    Reminder table, representing extra birthday reminders that a subscriber
+    can request for the birthday of a particular employee.
+    """
     async def addReminderFromUserAndEmployee(user, employee) -> Reminder:
+        """
+        Addition of a new reminder.
 
+        user - must be a User object.
+        employee - must be a Employee object.
+        """
         async with AppContext() as database:
             with database.atomic():
 
@@ -17,19 +27,36 @@ class ReminderController():
                 return new_reminder
 
     async def getReminderById(id) -> Reminder:
-            async with AppContext() as database:
-                with database.atomic():
+        """
+        Returns the Reminder object from the database that
+        matches the given id.
 
-                    return Reminder.get(Reminder.id == id)
+        id - id of Reminder.
+        """
+        async with AppContext() as database:
+            with database.atomic():
+
+                return Reminder.get(Reminder.id == id)
 
     async def getRemindersByUserId(user_id) -> Reminder:
+        """
+        Returns the Reminder object from the database that
+        matches the given user id.
+
+        id - id of the user.
+        """
         async with AppContext() as database:
             with database.atomic():
 
                 return Reminder.select().where(Reminder.user == user_id)
 
-
     async def deleteReminderById(id) -> bool:
+        """
+        Deletes the reminder that matches the reminder id from
+        the database. Returns true on success.
+
+        id - id of Reminder.
+        """
         async with AppContext() as database:
             with database.atomic():
 
@@ -37,8 +64,13 @@ class ReminderController():
                 reminder.delete_instance()
                 return True
 
-
     async def deleteRemindersByUserId(user_id) -> bool:
+        """
+        Deletes the reminders that match the user id from
+        the database. Returns true on success.
+
+        id - id of User.
+        """
         async with AppContext() as database:
             with database.atomic():
 
