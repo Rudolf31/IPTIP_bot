f888faf8dd93efaabb70c50588e8f7fdfe04c384
Rudolf31 <55095427+Rudolf31@users.noreply.github.com>
2024-09-29 21:17:09 +0300

    Revert "Revert "Controller logic""

--- database/controllers/user_controller.py
+++ database/controllers/user_controller.py
@@ -3,9 +3,73 @@ from database.application_context import User
 
 
 class UserController():
+    """
+    UserController for database actions related to User.
+    """
     async def addUser(user) -> User:
+        """
+        Addition of a new user.
+
+        user - must be a new User object.
+        """
+        async with AppContext() as database:
+            with database.atomic():
+
+                new_user = User.create(tg_id=user.tg_id, admin=user.admin)
+                new_user.save()
+                return new_user
+
+    async def getUserById(id) -> User:
+        """
+        Returns the User object from the database that
+        matches the given id.
+
+        id - id of User (do not confuse with Telegram ID)
+        """
 
         async with AppContext() as database:
-            new_user = User.create(tg_id=user.tg_id, admin=user.admin)
-            new_user.save()
-            return new_user
+            with database.atomic():
+
+                return User.get_or_none(User.id == id)
+
+    async def getUserByTgId(tg_id) -> User:
+        """
+        Returns the User object from the database that
+        matches the given Telegram user id.
+
+        id - id of the Telegram user.
+        """
+
+        async with AppContext() as database:
+            with database.atomic():
+
+                return User.get_or_none(User.tg_id == tg_id)
+
+    async def deleteUser(id) -> bool:
+        """
+        Deletes the user that matches the user id from
+        the database. Returns true on success.
+
+        id - id of User (do not confuse with Telegram ID)
+        """
+
+        async with AppContext() as database:
+            with database.atomic():
+
+                user = User.get(User.id == id)
+                user.delete_instance()
+                return True
+
+    async def updateUser(user) -> bool:
+        """
+        Updates the given user's parameters. Returns true
+        if successful.
+
+        user - User object
+        """
+
+        async with AppContext() as database:
+            with database.atomic():
+
+                user.save()
+                return True
--- services/bot_service.py
+++ services/bot_service.py
@@ -3,25 +3,31 @@ from aiogram.client.default import DefaultBotProperties
 from aiogram.enums import ParseMode
 from aiogram.filters import CommandStart
 from aiogram.types import Message
+
 from config import TOKEN
+from services.user_service import UserService
+
+
 # All handlers should be attached to the Router (or Dispatcher)
 dp = Dispatcher()
+
+
 @dp.message(CommandStart())
 async def command_start_handler(message: Message) -> None:
     """
     This handler receives messages with `/start` command
     """
-    # Most event objects have aliases for API methods that can be called in events' context
-    # For example if you want to answer to incoming message you can use `message.answer(...)` alias
-    # and the target chat will be passed to :ref:`aiogram.methods.send_message.SendMessage`
-    # method automatically or call API method directly via
-    # Bot instance: `bot.send_message(chat_id=message.chat.id, ...)`
+    if not await UserService.isUserRegistered(message.from_user):
+        await UserService.userRegister(message.from_user)
+
     await message.answer(f"Hello, {html.bold(message.from_user.full_name)}!")
+
+
 @dp.message()
 async def echo_handler(message: Message) -> None:
     """
     Handler will forward receive a message back to the sender
-    By default, message handler will handle all message types (like a text, photo, sticker etc.)
+    By default, message handler will handle all message types.
     """
     try:
         # Send a copy of the received message
@@ -32,10 +38,11 @@ async def echo_handler(message: Message) -> None:
 
 
 async def run_bot() -> None:
-    # Initialize Bot instance with default bot properties which will be passed to all API calls
-    bot = Bot(token=TOKEN, default=DefaultBotProperties(parse_mode=ParseMode.HTML))
+    # Initialize Bot instance with default bot properties
+    bot = Bot(
+        token=TOKEN,
+        default=DefaultBotProperties(parse_mode=ParseMode.HTML)
+    )
 
     # And the run events dispatching
     await dp.start_polling(bot)
-
-
