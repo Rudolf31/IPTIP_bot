f7f77a7d11b0ab9a33a207712cf6e2de8e9c909e
Nikolai Papin <nikolai@weirdcat.ru>
2024-10-02 20:10:58 +0300

    Reorganized birthday notification algorithm

--- services/distribution_service.py
+++ services/distribution_service.py
@@ -14,8 +14,10 @@ class DistributionService:
     year_seconds = 31556952
     day_seconds = 86400
 
+    #TODO: find out what this function actually returns
+    # (God I hate Python)
     @classmethod
-    def calculateNotificaionTime(cls, birth_date, reminder_day_offset):
+    def calculateNotificaionTime(cls, birth_date, reminder_day_offset) -> int:
         """
         Calculates the time of the next notification.
 
@@ -32,7 +34,7 @@ class DistributionService:
         return (current_year + extra_years) * cls.year_seconds + stripped_reminder_date
 
     @classmethod
-    def isNotificationDue(cls, target_date, reminder_day_offset):
+    def isNotificationDue(cls, target_date, reminder_day_offset) -> int:
         """
         Checks if the notification is due.
         Returns true if the notification is due.
@@ -57,34 +59,47 @@ class DistributionService:
     # possible to save reminders in the database (it uses timestamps afterall).
     # For now does not function at all
     @classmethod
-    async def employeeBirthdayNotification(cls, employee):
+    async def employeeBirthdayNotification(cls, employee) -> bool:
         """
         Sends a notification to the employee if time is due
         and schedules the next notification.
+        Returns true if the notification was sent.
 
         employee - must be an Employee object
         """
+        # Generated but will not necessarily be used
+        new_scheduled_reminder = cls.calculateNotificaionTime(employee.birthday, BIRTHDAY_NOTIFICATION_DAY_OFFSET)
+
+        # Employee has no reminder scheduled, let's fix it
         if employee.scheduled_reminder is None:
-            employee.scheduled_reminder = cls.calculateNotificaionTime(employee.birthday, BIRTHDAY_NOTIFICATION_DAY_OFFSET)
+            employee.scheduled_reminder = new_scheduled_reminder
             logger.info(f"{employee.full_name} ({employee.tg_id}) - reminder set to {employee.scheduled_reminder}")
-            return
+            return False
 
         due_state = cls.isNotificationDue(employee.scheduled_reminder, BIRTHDAY_NOTIFICATION_DAY_OFFSET)
 
+        # Too early, nothing to do
+        if due_state == 0:
+            return False
+
+        # Time to send the notification
         if due_state == 1:
 
             # Send notification in Telegram here
-            # If sent successfully, update the reminder time
+            # If not sent successfully, throw error
+            logger.info(f"{employee.full_name} ({employee.tg_id}) - birthday notifications supposed to be sent")
 
-            employee.scheduled_reminder = cls.calculateNotificaionTime(employee.birthday, BIRTHDAY_NOTIFICATION_DAY_OFFSET)
-            employee.save()
+        # We either got it in time or too late,
+        # let's schedule the next notification
+        employee.scheduled_reminder = new_scheduled_reminder
+        employee.save()
 
-        if due_state == -1:
-            employee.scheduled_reminder = cls.calculateNotificaionTime(employee.birthday, BIRTHDAY_NOTIFICATION_DAY_OFFSET)
-            employee.save()
+        logger.info(f"{employee.full_name} ({employee.tg_id}) - reminder set to {employee.scheduled_reminder}")
+
+        return False
 
     @classmethod
-    async def employeeBirthdayNotificationById(cls, id):
+    async def employeeBirthdayNotificationById(cls, id) -> bool:
         """
         Wrapper of employeeBirthdayNotification to be
         used with the id of the employee.
