ee5313282f75bd3ccb03f6a486211f4c29d4fc69
Nikolai Papin <nikolai@weirdcat.ru>
2024-10-04 17:15:16 +0300

    added db initialization to main

--- __main__.py
+++ __main__.py
@@ -4,9 +4,15 @@ import asyncio
 
 from services.bot_service import run_bot
 from services.distribution_service import DistributionService
+from database.application_context import AppContext
 
+ctx = None
 
 def main() -> None:
+
+    global ctx
+    ctx = AppContext()
+
     logging.basicConfig(level=logging.INFO, stream=sys.stdout)
     asyncio.run(DistributionService.birthdayCycle(forever=True, interval=600))
     asyncio.run(run_bot())
