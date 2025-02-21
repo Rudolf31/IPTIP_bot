aee5fb63bc70cd96c6b90b48b89821ed9d022289
unknown <rudolfshevchenko@gmail.com>
2025-02-21 13:26:30 +0300

    we have changed the time of birthday notifications for employees to 11am +- 3 hours

--- services/distribution_service.py
+++ services/distribution_service.py
@@ -83,7 +83,7 @@ class DistributionService:
 
         if now() > notification_due_date:
             return -1  # Late
-        elif (now() > target_date) and abs(now().hour - target_date.hour) < 3:
+        elif (now() > target_date) and abs(now().hour - 11) < 3:
             return 1  # On time
         else:
             return 0  # Early
